9f3e4e5d3359c28fa4354caf74936832361e46b7
Winctran <noreply@github.com>
2025-11-17 08:53:00 +0100

    Add shell script for packing and copying the pkg files

---
+++ pack_package.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+location=~/.config/r2modmanPlus-local/HadesII/profiles/Default/ReturnOfModding/
+plugins=$location/plugins/Winctran-Nude-Melinoe
+plugins_data=$location/plugins_data/Winctran-Nude-Melinoe
+
+~/.local/bin/deppth2 pk -s ~/Code/Nude-Melinoe/package_src -t $plugins_data/NudeMelinoe.pkg
