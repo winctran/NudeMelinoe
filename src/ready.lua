9049c71ef8c76e15e71be399d2df641f150d4c56
Winctran <noreply@github.com>
2025-11-25 06:23:37 +0100

    Add explicit reskin

--- src/ready.lua
+++ src/ready.lua
@@ -7,11 +7,31 @@
 -- 	so you will most likely want to have it reference
 --	values and functions later defined in `reload.lua`.
 
-modutil.mod.Path.Wrap("SetupMap", function(base)
-    LoadPackages({
-        Names = {
-            "NudeMelinoe"
-        }
+local texture_name = "NudeMelinoe/MelinoeSkin"
+
+if CurrentRun ~= nil then
+    SetThingProperty({
+        Property = "GrannyTexture",
+        Value = texture_name,
+        DestinationId = CurrentRun.Hero.ObjectId
     })
+end
+
+modutil.mod.Path.Wrap("SetThingProperty", function(base, args)
+    if CurrentRun.Hero.SubtitleColor ~= Color.ChronosVoice and
+        (MapState.HostilePolymorph == false or MapState.HostilePolymorph == nil) and
+        args.Property == "GrannyTexture" and
+        (args.Value == "null" or args.Value == "") and
+        args.DestinationId == CurrentRun.Hero.ObjectId then
+        args_copy = DeepCopyTable(args)
+        args_copy.Value = texture_name
+        base(args_copy)
+    else
+        base(args)
+    end
+end)
+
+modutil.mod.Path.Wrap("SetupMap", function(base)
+    LoadPackages({ Name = { "NudeMelinoe" } })
     base()
 end)
